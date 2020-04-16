e0ad90302c0c9b492d6dabdd7b824708ee264af0
Alexey Gogolev <a.gogolev@mashtab.org>
2020-04-16 18:51:06 +0300

    FEATURE TG-7975 Change jenkins pipeline syntax to declarative-7

--- devops/pipeline/vdi-connect-dec.groovy
+++ devops/pipeline/vdi-connect-dec.groovy
@@ -11,13 +11,13 @@ pipeline {
     post {
         failure {
             println "Something goes wrong"
-            println "Current build marked as ${currentBuild.result}: ${err}"
+            println "Current build marked as ${currentBuild.result}"
             notifyBuild(slackNotify,"FAILED", "Something goes wrong. Version: ${currentDate}")
         }
 
         aborted {
             println "Build was interrupted manually"
-            println "Current build marked as ${currentBuild.result}: ${err}"
+            println "Current build marked as ${currentBuild.result}"
             notifyBuild(slackNotify,"FAILED", "Build was interrupted manually. Version: ${currentDate}")
         }
 
