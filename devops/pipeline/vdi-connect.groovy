773008555aa129ce80c6c10637a85354464de484
Solomin <a.solomin@mashtab.org>
2020-06-30 17:33:15 +0300

    FEATURE TG-8910 Добавлено слово api во все урлы запросов как того хочет nginx

--- devops/pipeline/vdi-connect.groovy
+++ devops/pipeline/vdi-connect.groovy
@@ -35,7 +35,7 @@ pipeline {
 
     parameters {
         string(      name: 'BRANCH',               defaultValue: 'master',          description: 'branch')
-        string(      name: 'VERSION',              defaultValue: '1.2.10',          description: 'version')
+        string(      name: 'VERSION',              defaultValue: '1.2.11',          description: 'version')
         booleanParam(name: 'DEBIAN',               defaultValue: true,              description: 'create DEB?')
         booleanParam(name: 'UBUNTU',               defaultValue: true,              description: 'create DEB?')
         booleanParam(name: 'CENTOS7',              defaultValue: true,              description: 'create RPM?')
