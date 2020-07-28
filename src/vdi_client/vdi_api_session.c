f4f0f2353e07c11be1941416f46a8359e1c34017
Solomin <a.solomin@mashtab.org>
2020-07-28 14:52:02 +0300

    FEATURE_TG-9246 Client-Type to User-Agent

--- src/vdi_client/vdi_api_session.c
+++ src/vdi_client/vdi_api_session.c
@@ -39,7 +39,7 @@ static void setup_header_for_api_call(SoupMessage *msg)
     soup_message_headers_append(msg->request_headers, "Authorization", authHeader);
     g_free(authHeader);
     soup_message_headers_append(msg->request_headers, "Content-Type", "application/json");
-    soup_message_headers_append(msg->request_headers, "Client-Type", "thin-client");
+    soup_message_headers_append(msg->request_headers, "User-Agent", "thin-client");
 }
 
 static guint send_message(SoupMessage *msg)
@@ -68,7 +68,7 @@ static gboolean vdi_api_session_get_token()
 
     // set header
     soup_message_headers_append(msg->request_headers, "Content-Type", "application/json");
-    soup_message_headers_append(msg->request_headers, "Client-Type", "thin-client");
+    soup_message_headers_append(msg->request_headers, "User-Agent", "thin-client");
 
     // set body
     gchar *ldap_str = vdiSession.is_ldap ? g_strdup("true") : g_strdup("false");
