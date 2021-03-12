0533380f4a0d1f8c2a2190495d775a1886f972df
Alexey Gogolev <a.gogolev@mashtab.org>
2021-03-12 16:18:10 +0300

    FEATURE TG-12251 Build for alt linux-2

--- devops/veil-connect-linux-installer.sh
+++ devops/veil-connect-linux-installer.sh
@@ -60,6 +60,7 @@ EOF
         apt-get update && apt-get install wget -y
         wget https://veil-update.mashtab.org/veil-connect/linux/apt-rpm/x86_64/RPMS.alt9/veil-connect-latest.rpm
         apt-get install ./veil-connect-latest.rpm -y
+        rm -f veil-connect-latest.rpm
         ;;
     *)
         echo "Error: Empty OS" ;;
