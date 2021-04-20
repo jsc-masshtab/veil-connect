c09c9376e2074b3417dc00d6267f9b8835c3c3fa
Solomin <a.solomin@mashtab.org>
2021-04-20 11:10:00 +0300

    Merge feature_tg_12918 into dev

--- src/main.c
+++ src/main.c
@@ -26,60 +26,14 @@
 #include "settingsfile.h"
 #include "remote-viewer.h"
 #include "remote-viewer-util.h"
-#include "crashhandler.h"
-#include "vdi_session.h"
-#include "usbredir_controller.h"
+#include "veil_logger.h"
 
-void
-setup_logging()
-{
-    gchar *log_dir = get_log_dir_path();
-    // create log dir
-    g_mkdir_with_parents(log_dir, 0755);
-
-    // Return if we cant write to log dir
-    if (g_access(log_dir, W_OK) != 0) {
-        printf("Log directory is protected from writing\n");
-        g_free(log_dir);
-        return;
-    }
-
-    // get ts
-    GDateTime *datetime = g_date_time_new_now_local();
-    gchar *data_time_string = g_date_time_format(datetime, "%Y_%m_%d___%H_%M_%S");
-    // printf("data_time_string %s", data_time_string);
-    g_date_time_unref(datetime);
-
-    // crash handler
-    gchar *bt_file_name = g_strconcat(log_dir, "/", data_time_string, "_backtrace.txt", NULL);
-    convert_string_from_utf8_to_locale(&bt_file_name);
-    install_crash_handler(bt_file_name);
-
-    //error output
-    gchar *stderr_file_name = g_strconcat(log_dir, "/", data_time_string, "_stderr.txt", NULL);
-    convert_string_from_utf8_to_locale(&stderr_file_name);
-    FILE *err_file_desc = freopen(stderr_file_name, "w", stderr);
-    (void)err_file_desc;
-
-    //stdout output
-    gchar *stdout_file_name = g_strconcat(log_dir, "/", data_time_string, "_stdout.txt", NULL);
-    convert_string_from_utf8_to_locale(&stdout_file_name);
-    FILE *out_file_desc = freopen(stdout_file_name, "w", stdout);
-    (void)out_file_desc; // it would be polite to close file descriptors
-
-    // free memory
-    g_free(data_time_string);
-    g_free(bt_file_name);
-    g_free(stderr_file_name);
-    g_free(stdout_file_name);
-    g_free(log_dir);
-}
 
 int
 main(int argc, char **argv)
 {
 #ifdef NDEBUG // logging errors and traceback in release mode
-    setup_logging();
+    veil_logger_setup();
 #endif
     // disable stdout buffering
     setbuf(stdout, NULL);
@@ -101,6 +55,7 @@ main(int argc, char **argv)
     remote_viewer_free_resources(remote_viewer);
     g_object_unref(remote_viewer);
     free_ini_file_name();
+    veil_logger_free();
 
     return ret;
 }
---
+++ src/utility/veil_logger.c
@@ -0,0 +1,127 @@
+//
+// Created by ubuntu on 19.04.2021.
+//
+
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <config.h>
+#include "veil_logger.h"
+#include "remote-viewer-util.h"
+#include "crashhandler.h"
+#include "settingsfile.h"
+
+#define MAX_STRING_LENGTH 10485761 // 1024 X 1024
+
+static FILE *err_file_desc = NULL;
+static FILE *out_file_desc = NULL;
+static gchar *cur_log_path = NULL;
+static gchar *prev_clipboard_text = NULL;
+static gint clipboard_log_enabled = 1;
+
+void veil_logger_setup()
+{
+    // read settings from ini
+    clipboard_log_enabled = read_int_from_ini_file("General", "clipboard_log_enabled", 1);
+
+    g_autofree gchar *log_dir = NULL;
+    log_dir = get_log_dir_path();
+
+    // current log dir
+    GDateTime *datetime = g_date_time_new_now_local();
+    g_autofree gchar *data_time_string = NULL;
+    data_time_string = g_date_time_format(datetime, "%Y_%m_%d___%H_%M_%S");
+    g_date_time_unref(datetime);
+    cur_log_path = g_build_filename(log_dir, data_time_string, NULL);
+
+    // create log dir
+    g_mkdir_with_parents(cur_log_path, 0755);
+
+    // Return if we cant write to log dir
+    if (g_access(cur_log_path, W_OK) != 0) {
+        g_warning("Log directory is protected from writing");
+        return;
+    }
+
+    // crash handler
+    gchar *bt_file_name = g_build_filename(cur_log_path, "backtrace.txt", NULL);
+    convert_string_from_utf8_to_locale(&bt_file_name);
+    install_crash_handler(bt_file_name);
+
+    //error output
+    gchar *stderr_file_name = g_build_filename(cur_log_path, "stderr.txt", NULL);
+    convert_string_from_utf8_to_locale(&stderr_file_name);
+    err_file_desc = freopen(stderr_file_name, "w", stderr);
+
+    //stdout output
+    gchar *stdout_file_name = g_build_filename(cur_log_path, "stdout.txt", NULL);
+    convert_string_from_utf8_to_locale(&stdout_file_name);
+    out_file_desc = freopen(stdout_file_name, "w", stdout);
+
+    // free memory
+    g_free(bt_file_name);
+    g_free(stderr_file_name);
+    g_free(stdout_file_name);
+}
+
+void veil_logger_free()
+{
+    if(err_file_desc)
+        fclose(err_file_desc);
+    if(out_file_desc)
+        fclose(out_file_desc);
+    g_free(cur_log_path);
+    g_free(prev_clipboard_text);
+}
+
+void logger_save_clipboard_data(const gchar *data, guint size, ClipboardLoggerDataType data_type)
+{
+    if (!clipboard_log_enabled)
+        return;
+
+    // open file
+    g_autofree gchar *file_name = NULL;
+    file_name = g_build_filename(cur_log_path, "clipboard.txt", NULL);
+    FILE *file = fopen(file_name, "a");
+
+    if(!file)
+        return;
+
+    // trim and compare
+    guint final_size = MIN(size, MAX_STRING_LENGTH);
+    gchar *clipboard_text = g_strndup(data, final_size);
+
+    if (prev_clipboard_text == NULL || strcmp(clipboard_text, prev_clipboard_text) != 0) {
+
+        // write time
+        GDateTime *datetime = g_date_time_new_now_local();
+        g_autofree gchar *data_time_string = NULL ;
+        data_time_string = g_date_time_format(datetime, "%T");
+        g_date_time_unref(datetime);
+
+        fwrite(data_time_string, sizeof(gchar), strlen(data_time_string), file);
+
+        // write type
+        if (data_type == CLIPBOARD_LOGGER_FROM_CLIENT_TO_VM) {
+            const gchar *type = " FROM_CLIENT_TO_VM:  ";
+            fwrite(type, sizeof(gchar), strlen(type), file);
+
+        } else if (data_type == CLIPBOARD_LOGGER_FROM_VM_TO_CLIENT) {
+            const gchar *type = " FROM_VM_TO_CLIENT:  ";
+            fwrite(type, sizeof(gchar), strlen(type), file);
+        }
+
+        // write text
+        fwrite(clipboard_text, sizeof(gchar), final_size, file);
+        putc ('\n' , file);
+    }
+
+    g_free(prev_clipboard_text);
+    prev_clipboard_text = clipboard_text;
+
+    // close file
+    fclose(file);
+}
