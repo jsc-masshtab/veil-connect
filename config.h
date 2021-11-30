016d9b9fc93f84461db6aba63fa95c5f7ef4b37e
a.solomin <a.solomin@mashtab.org>
2021-11-30 16:59:13 +0300

    Patch bump

--- config.h
+++ config.h
@@ -106,7 +106,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.8.9"
+#define PACKAGE_VERSION "1.8.10"
 
 /* OS ID for this build */
 /* #undef REMOTE_VIEWER_OS_ID */
