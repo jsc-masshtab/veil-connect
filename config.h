3e9fa050eda9ab9d1dcbdcb8003e3855542be45a
Solomin <a.solomin@mashtab.org>
2022-05-20 11:53:55 +0300

    FEATURE TG-16761 Minor version bump

--- config.h
+++ config.h
@@ -107,7 +107,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.12.0"
+#define PACKAGE_VERSION "1.13.0"
 
 /* OS ID for this build */
 /* #undef REMOTE_VIEWER_OS_ID */
