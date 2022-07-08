91046c8bb73d8361fadc1c5abdab4be6a749efab
Solomin <a.solomin@mashtab.org>
2022-07-08 10:01:08 +0300

    FIX TG-17344 Patch version bump

--- config.h
+++ config.h
@@ -107,7 +107,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.14.0"
+#define PACKAGE_VERSION "1.14.1"
 
 /* OS ID for this build */
 /* #undef REMOTE_VIEWER_OS_ID */
--- devops/jenkins/vars/buildParameters.groovy
+++ devops/jenkins/vars/buildParameters.groovy
@@ -3,7 +3,7 @@ def branch() {
 }
 
 def version() {
-    return "1.14.0"
+    return "1.14.1"
 }
 
 def linuxAgents() {
